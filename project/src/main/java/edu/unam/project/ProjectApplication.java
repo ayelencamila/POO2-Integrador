a0fdaa780c1b95e4032bcde36b19fc1caa26747d
Leonardelli-FCEQYN <lisandro.leonardelli@fceqyn.unam.edu.ar>
2024-11-08 08:53:24 -0300

    Creación del proyecto

---
+++ project/src/main/java/edu/unam/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.unam.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
